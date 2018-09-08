a4c84d5393cd7c440d96a0e91828583cae106d1a
Krzysztof Sroga <krzysztof@sroga.pw>
2018-09-09 02:20:49 +0300

    printing all, cleanall

--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -1,3 +1,5 @@
+import kotlin.coroutines.experimental.buildSequence
+
 object Utils {
     class Int12(bits: Int) {
         val first6 = bits shr 6
@@ -6,10 +8,15 @@ object Utils {
 
     const val base64characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     const val hexCharacters = "0123456789abcdef"
+    val byteHexCharacters = buildSequence {
+        for (i in 0..255) {
+            yield(String.format("%02x", i))
+        }
+    }
 
     private fun splitIntoLongs(string: String): List<Long> {
         var list = listOf<Long>()
-        for (i in 0 until string.length step 4) {
+        for (i in 0 .. string.length-4 step 4) {
             list += string.slice(i until i + 4).toLong(16)
         }
         return list
@@ -20,12 +27,12 @@ object Utils {
     }
 
     fun xorString(string1: String, string2: String): String {
-        return xorList(splitIntoLongs(string1), splitIntoLongs(string2)).joinToString(separator = "") { String.format("%02x", it) }
+        return xorList(splitIntoLongs(string1), splitIntoLongs(string2)).joinToString(separator = "") { String.format("%04x", it) }
     }
 
     fun base64(string: String): String {
         var outputString = ""
-        for (i in 0 until string.length step 3) {
+        for (i in 0 .. string.length-3 step 3) {
             val int12 = Int12(string.slice(i..i + 2).toInt(16))
             outputString += Utils.base64characters[int12.first6]
             outputString += Utils.base64characters[int12.last6]
