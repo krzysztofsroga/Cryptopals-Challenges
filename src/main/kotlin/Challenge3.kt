774e68010497e32ed94c954352c7367ce26ad36a
Krzysztof Sroga <krzysztof@sroga.pw>
2018-09-09 14:29:53 +0300

    added letter and character statistics

--- src/main/kotlin/Challenge3.kt
+++ src/main/kotlin/Challenge3.kt
@@ -1,23 +1,8 @@
 class Challenge3(val hexString: HexString) {
-    private val letterStats = mapOf(
-            'a' to 0.08167, 'b' to 0.01492,
-            'c' to 0.02782, 'd' to 0.04253,
-            'e' to 0.12702, 'f' to 0.02228,
-            'g' to 0.02015, 'h' to 0.06094,
-            'i' to 0.06966, 'j' to 0.00153,
-            'k' to 0.00772, 'l' to 0.04025,
-            'm' to 0.02406, 'n' to 0.06749,
-            'o' to 0.07507, 'p' to 0.01929,
-            'q' to 0.00095, 'r' to 0.05987,
-            's' to 0.06327, 't' to 0.09056,
-            'u' to 0.02758, 'v' to 0.00978,
-            'w' to 0.02360, 'x' to 0.00150,
-            'y' to 0.01974, 'z' to 0.00074,
-            ' ' to 0.2
-    )
+
 
     private fun scoreString(string: String) = string.toLowerCase().map { character ->
-        letterStats[character] ?: 0.0
+        Utils.characterStats[character] ?: 0.0
     }.sum()
 
     private fun xorWithRepeated(string: HexString, toRepeat: HexString): HexString {
@@ -36,12 +21,6 @@ class Challenge3(val hexString: HexString) {
             result.score
         }!!
         println("Best matching character is: ${bestMatch.character} which scored ${bestMatch.score} and produced sentence: '${bestMatch.string}'")
-//        println("All other strings: ")
-//        println(
-//                Utils.byteHexCharacters.map { character ->
-//                    Utils.toAsciiString(xorWithRepeated(hexString, character))
-//                }.joinToString(separator = "\n")
-//        )
     }
 
 }
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -2,6 +2,26 @@ import kotlin.coroutines.experimental.buildSequence
 
 object Utils {
 
+    val letterStats = mapOf(
+            'a' to 0.08167, 'b' to 0.01492, 'c' to 0.02782, 'd' to 0.04253,
+            'e' to 0.12702, 'f' to 0.02228, 'g' to 0.02015, 'h' to 0.06094,
+            'i' to 0.06966, 'j' to 0.00153, 'k' to 0.00772, 'l' to 0.04025,
+            'm' to 0.02406, 'n' to 0.06749, 'o' to 0.07507, 'p' to 0.01929,
+            'q' to 0.00095, 'r' to 0.05987, 's' to 0.06327, 't' to 0.09056,
+            'u' to 0.02758, 'v' to 0.00978, 'w' to 0.02360, 'x' to 0.00150,
+            'y' to 0.01974, 'z' to 0.00074
+    ) //According to Wikipedia
+
+    val characterStats = mapOf(
+            ' ' to 0.1831685753, 'e' to 0.1021787708, 't' to 0.0750999398, 'a' to 0.0655307059,
+            'o' to 0.0620055405, 'n' to 0.0570308374, 'i' to 0.0573425524, 's' to 0.0532626738,
+            'r' to 0.0497199926, 'h' to 0.0486220925, 'l' to 0.0335616550, 'd' to 0.0335227377,
+            'u' to 0.0229520040, 'c' to 0.0226508836, 'm' to 0.0201727037, 'f' to 0.0197180888,
+            'w' to 0.0168961396, 'g' to 0.0163586607, 'p' to 0.0150311560, 'y' to 0.0146995463,
+            'b' to 0.0127076566, 'v' to 0.0078804815, 'k' to 0.0056916712, 'x' to 0.0014980832,
+            'j' to 0.0011440544, 'q' to 0.0008809302, 'z' to 0.0005979301
+    ) // from http://www.macfreek.nl/memory/Letter_Distribution#Letter_Frequency
+
     class Int12(bits: Int) {
         val first6 = bits shr 6
         val last6 = bits and 0x3f
@@ -17,7 +37,7 @@ object Utils {
 
     private fun splitIntoLongs(string: HexString): List<Long> {
         var list = listOf<Long>()
-        for (i in 0 .. string.length-4 step 4) {
+        for (i in 0..string.length - 4 step 4) {
             list += string.hex.slice(i until i + 4).toLong(16)
         }
         return list
@@ -35,7 +55,7 @@ object Utils {
 
     fun base64(hexString: HexString): Base64String {
         var outputString = ""
-        for (i in 0 .. hexString.length-3 step 3) {
+        for (i in 0..hexString.length - 3 step 3) {
             val int12 = Int12(hexString.hex.slice(i..i + 2).toInt(16))
             outputString += Utils.base64characters[int12.first6]
             outputString += Utils.base64characters[int12.last6]
@@ -49,8 +69,8 @@ object Utils {
 
     private fun splitIntoChars(hexString: HexString): CharArray {
         var list = listOf<Char>()
-        for (i in 0 .. hexString.length - 2 step 2) {
-            list += hexString.hex.slice(i..i+1).toInt(16).toChar()
+        for (i in 0..hexString.length - 2 step 2) {
+            list += hexString.hex.slice(i..i + 1).toInt(16).toChar()
         }
         return list.toCharArray()
     }
