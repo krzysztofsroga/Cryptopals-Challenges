6caa663d2d29c02a3e2caa4996aa89b4c00b1d21
Krzysztof Sroga <krzysztof@sroga.pw>
2018-09-12 02:17:00 +0300

    some fixes

--- src/main/kotlin/AdvancedUtils.kt
+++ src/main/kotlin/AdvancedUtils.kt
@@ -1,8 +1,10 @@
+import java.lang.IllegalArgumentException
 import java.math.BigInteger
 import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 import kotlin.experimental.xor
 import java.nio.charset.Charset
+import java.util.*
 
 val charset: Charset = StandardCharsets.UTF_8
 
@@ -36,3 +38,31 @@ infix fun String.xor(other: String): String {
 
 fun String.bitCount(): Int = BigInteger(toByteArray(charset)).bitCount()
 
+val Base64String.decoded: String
+    get() {
+        return Base64.getDecoder().decode(base64).toString(StandardCharsets.UTF_8)
+
+//        base64.map { base64character ->
+//            val index = Utils.base64characters.toCharArray().find { it == base64character }!!
+//
+//        }
+
+    }
+
+fun <T, R> Collection<T>.mapPairs(transform: (T, T) -> R): List<R> {
+    if ((size and 0x01) != 0)
+        throw IllegalArgumentException("Argument has odd number of items: $size")
+    val list = mutableListOf<R>()
+    for (i in 0 until size step 2) {
+        list += transform(elementAt(i), elementAt(i + 1))
+    }
+    return list
+}
+
+fun <R> String.mapSlices(sliceLength: Int, transform: (String) -> R): List<R> {
+    val list = mutableListOf<R>()
+    for (i in 0 until length step sliceLength) {
+        list += transform(slice(i until i + sliceLength))
+    }
+    return list
+}
--- src/main/kotlin/Challenge6.kt
+++ src/main/kotlin/Challenge6.kt
@@ -1,5 +1,13 @@
-class Challenge6 {
+import java.io.File
+import java.nio.charset.StandardCharsets
+import java.util.*
 
+class Challenge6(private val file: File) {
+    fun breakKeysize() {
+        file.forEachLine { line ->
+            Base64String(line).decoded
+        }
+    }
 }
 
 fun main(args: Array<String>) {
@@ -7,4 +15,11 @@ fun main(args: Array<String>) {
     val string1 = "this is a test"
     val string2 = "wokka wokka!!!"
     println("The distance between '$string1' and '$string2' is ${(string1 xor string2).bitCount()}")
+
+    val challenge = Challenge6(File("src/main/resources/Challenge6.txt"))
+
+    val dupa = "eonhudnaoduergadoeu132"
+    val encoded_dupa = Base64.getEncoder().encode(dupa.toByteArray()).toString(StandardCharsets.UTF_8).asBase64String()
+    println(encoded_dupa.decoded)
+
 }
