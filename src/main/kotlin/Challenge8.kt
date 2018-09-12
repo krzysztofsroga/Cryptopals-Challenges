571c6c50902dfdf39eea5a0fd7ad36ced995cac0
Krzysztof Sroga <krzysztof@sroga.pw>
2018-09-12 15:49:59 +0300

    fixed ByteArray.toSlices

--- src/main/kotlin/Challenge8.kt
+++ src/main/kotlin/Challenge8.kt
@@ -15,26 +15,61 @@ object ECBAnalyzer {
         val blocksCount = cipher.size / blockSize
         var matches = 0
 
+        println("ups ups ups ups ups ups ups ups ups ups ups ups ups ups ups ups")
+        println("BlocksCount = $blocksCount (divided ${cipher.size} over $blockSize")
+        println("SlicesCount = ${cipher.toSlices(blockSize).size}")
+        println("Slices: ${cipher.toSlices(blockSize)}")
+
         for (index in 0 until blocksCount) {
+            println("HAHA $index")
             val block = cipher.extractBlock(blockSize, index)
             for (j in index + 1 until blocksCount) {
+                println("i = $index, j = $j")
+                val other = cipher.extractBlock(blockSize, j)
+//                if (block.contentEquals(other))
+//                    matches++
+            }
+        }
+        println("ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO ELOELO")
+
+        cipher.toSlices(blockSize).forEachIndexed { index, block ->
+            println("HAHA $index")
+            for (j in index + 1 until blocksCount) {
+                println("i = $index, j = $j")
                 val other = cipher.extractBlock(blockSize, j)
                 if (block.contentEquals(other))
                     matches++
             }
         }
 
+        println("---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----")
+
+        cipher.toSlices(blockSize).forEachIndexed { index, block ->
+            println("HAHA $index")
+            cipher.toSlices(blockSize).forEachIndexed { j, other ->
+                if (j > index) {
+                    println("i = $index, j = $j")
+//                    if(block.contentEquals(other))
+//                        matches++
+                }
+
+            }
+        }
+
+
+
+
         return matches
     }
 
-    private fun ByteArray.toSlices(sliceSize: Int): List<ByteArray> {
-        return (0..size step sliceSize).map { i ->
-            sliceArray(i..min(i + sliceSize, size - 1))
+    private fun ByteArray.toSlices(sliceSize: Int): List<ByteArray> { //TODO this function is broken, returns one more slice!
+        return (0 until size step sliceSize).map { i ->
+            sliceArray(i until min(i + sliceSize, size - 1))
         }
 
 //        val slices = mutableListOf<ByteArray>()
-//        for (i in 0..size step sliceSize) {
-//            slices += sliceArray(i..min(i + sliceSize, size - 1))
+//        for (i in 0 until size step sliceSize) {
+//            slices += sliceArray(i until min(i + sliceSize, size - 1))
 //        }
 //        return slices
     }
