c942c108c83c2728dbe932d05355235588f26ab9
Krzysztof Sroga <krzysztof@sroga.pw>
2018-09-12 11:25:26 +0300

    challenge 6 completed

--- src/main/kotlin/Challenge6.kt
+++ src/main/kotlin/Challenge6.kt
@@ -1,40 +1,35 @@
 import java.io.File
 import java.lang.StringBuilder
-import java.nio.charset.StandardCharsets
-import java.util.*
 
-class BFR(val string: String, val score: Double)
+class BFR(val score: Double, val string: String)
 
 
 class Challenge6(file: File) {
     private val fileData = readAndDecodeFile(file)
 
-    fun breakKeysize(): Int {
-        printBestKeySizes()
-        return (2..41).map { keysize -> Pair(keysize, scoreKeysize(keysize)) }.minBy { it.second }!!.first
+    fun breakBestKeySizes(): List<Int> {
+        return (2..41).map { keySize -> Pair(keySize, scoreKeysize(keySize)) }.sortedBy { it.second }.map { it.first }
     }
 
     fun printBestKeySizes() {
-        println((2..41).map { keysize -> Pair(keysize, scoreKeysize(keysize)) }.sortedBy { it.second })
+        println((2..41).map { keySize -> Pair(keySize, scoreKeysize(keySize)) }.sortedBy { it.second })
     }
 
-    private fun scoreKeysize(keysize: Int): Double {
-        val first = fileData.slice(0 until keysize)
-        val second = fileData.slice(keysize until 2 * keysize)
-        val third = fileData.slice(2 * keysize until 3 * keysize)
-        val fourth = fileData.slice(3 * keysize until 4 * keysize)
-        return ((first xor second).bitCount() + (second xor third).bitCount() + (third xor fourth).bitCount()).toDouble() / keysize //TODO use ZipWithNext
+    private fun scoreKeysize(keySize: Int): Double {
+        val first = fileData.slice(0 until keySize)
+        val second = fileData.slice(keySize until 2 * keySize)
+        val third = fileData.slice(2 * keySize until 3 * keySize)
+        val fourth = fileData.slice(3 * keySize until 4 * keySize)
+        return ((first xor second).bitCount() + (second xor third).bitCount() + (third xor fourth).bitCount()).toDouble() / keySize //TODO use ZipWithNext
     }
 
-    fun solveForKeySize(keysize: Int): String {
-        val blocks = fileData.asSlices(keysize).transposed().map {column ->
+    fun solveForKeySize(keySize: Int): String {
+        return fileData.asSlices(keySize).transposed().map { column ->
             (0..255).map { byte ->
                 val string = column xorRepeated byte.toChar().toString()
-                BFR(string, Utils.scoreStringByCharacters(string))
+                BFR(Utils.scoreStringByCharacters(string), string)
             }.maxBy { result -> result.score }!!.string
         }.transposed().joinToString(separator = "")
-
-        return blocks
     }
 
     private fun readAndDecodeFile(file: File): String {
@@ -50,17 +45,18 @@ fun main(args: Array<String>) {
     // Simple test
     val string1 = "this is a test"
     val string2 = "wokka wokka!!!"
-    println("The distance between '$string1' and '$string2' is ${(string1 xor string2).bitCount()}")
+    println("The distance between '$string1' and '$string2' is ${(string1 xor string2).bitCount()}\n")
 
     val challenge = Challenge6(File("src/main/resources/Challenge6.txt"))
-    val bestKeySize = challenge.breakKeysize()
-    println("Optimal keysize is $bestKeySize")
+    val bestKeySizes = challenge.breakBestKeySizes().take(5) //takes 5 best results and proceeds with them
+    println("Best keySizes are $bestKeySizes\n")
 //    println(challenge.solveForKeySize(bestKeySize))
-    println(challenge.solveForKeySize(29))
+    val bestText = bestKeySizes.map { challenge.solveForKeySize(it) }.map { BFR(Utils.scoreStringByCharacters(it), it) }.sortedBy { it.score }.last().string
+    println("Best promising one is:\n\n $bestText")
+//    println(challenge.solveForKeySize(29))
 
 //    (2..41).forEach {i->
-//        println("Solution for keysize $i:\n${challenge.solveForKeySize(i)}\n\n\n")
+//        println("Solution for keySize $i:\n${challenge.solveForKeySize(i)}\n\n\n")
 //    }
-    println(arrayOf(string1, string2).toList().transposed())
 
 }
