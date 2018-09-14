3fb77bd5fa00130f6b3eba22b0034ae87dec1ebe
Krzysztof Sroga <krzysztof@sroga.pw>
2018-09-14 03:14:21 +0300

    simplified challenge 7

--- src/main/kotlin/Challenge7.kt
+++ src/main/kotlin/Challenge7.kt
@@ -7,26 +7,19 @@ import org.apache.shiro.codec.CodecSupport
 import java.io.File
 import java.nio.charset.StandardCharsets
 
-class AESECB {
-    fun decrypt(ciphertext: ByteArray, key: ByteArray): String {
-        val cipher = AesCipherService()
-        cipher.setMode(OperationMode.ECB)
-        cipher.setPaddingScheme(PaddingScheme.NONE)
+class Challenge7(file: File) {
+    private val fileData = Base64.decode(file.readLines(StandardCharsets.UTF_8).joinToString(""))!!
 
-        val raw = cipher.decrypt(ciphertext, key).bytes
+    fun decrypt(key: String): String {
+        val aesService = AesCipherService().
+        aesService.setMode(OperationMode.ECB)
+        aesService.setPaddingScheme(PaddingScheme.NONE)
+
+        val raw = aesService.decrypt(fileData, key.toByteArray(StandardCharsets.UTF_8)).bytes
         return raw.dropLast(raw.last().toInt()).toByteArray().toString(StandardCharsets.UTF_8)
     }
 }
 
-class Challenge7(file: File) {
-    val fileData = file.readLines(StandardCharsets.UTF_8).joinToString("")
-    fun decryptAES128ECB(key: String): String {
-        val aesecb = AESECB()
-        val plaintext = aesecb.decrypt(Base64.decode(fileData), CodecSupport.toBytes(key))
-        return plaintext
-    }
-}
-
 fun main(args: Array<String>) {
-    println(Challenge7(File("src/main/resources/Challenge7.txt")).decryptAES128ECB("YELLOW SUBMARINE"))
+    println(Challenge7(File("src/main/resources/Challenge7.txt")).decrypt("YELLOW SUBMARINE"))
 }
