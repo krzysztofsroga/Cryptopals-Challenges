8ae3769ee39c814c7a93d6e00a802c1afe254815
Krzysztof Sroga <krzysztof@sroga.pw>
2018-10-25 12:42:19 +0300

    added documentation

--- src/main/kotlin/Challenge7.kt
+++ src/main/kotlin/Challenge7.kt
@@ -1,3 +1,12 @@
+/**
+ * AES in ECB mode
+ * The Base64-encoded content in this file has been encrypted via AES-128 in ECB mode under the key
+ * "YELLOW SUBMARINE".
+ * (case-sensitive, without the quotes; exactly 16 characters; I like "YELLOW SUBMARINE" because it's exactly 16 bytes long, and now you do too).
+ * Decrypt it. You know the key, after all.
+ * Easiest way: use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
+ */
+
 import org.apache.shiro.crypto.AesCipherService
 import org.apache.shiro.crypto.OperationMode
 
